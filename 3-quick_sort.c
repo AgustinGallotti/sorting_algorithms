1c1383d163e9cdb1f14696c723d0eba5ed81b131
AgustinGallotti <Agustingallotti23@gmail.com>
2022-06-13 14:58:24 -0700

    commit

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -64,7 +64,7 @@ void quicksort(int *array, size_t size, ssize_t low, ssize_t hig)
 */
 void quick_sort(int *array, size_t size)
 {
-	if (!array || !size)
+	if (!array || size < 2)
 		return;
 	quicksort(array, size, 0, size - 1);
 }
